68efd77c0160aff0e325f251b95643a1903bbbe8
Preetika Prakash <preetikaprakash3103@gmail.com>
2022-02-15 23:56:44 +0530

    Adding the search in sorted array solution

---
+++ src/searching/search_in_a_sorted_array.java
@@ -0,0 +1,41 @@
+package searching;
+
+import java.util.Scanner;
+
+public class search_in_a_sorted_array {
+    static int searchInSorted(int arr[], int N, int K)
+    {
+        // Applying Binary Search
+        int low,high=0;
+        low=0;
+        high=N-1;
+        while(low<high) {
+            int mid = (low + high) / 2;
+            if (arr[mid] == K) {
+                return 1;
+            } else if (arr[mid] > K) {
+                high = mid - 1;
+            } else {
+                low = mid + 1;
+            }
+        }
+        return -1;
+    }
+    public static void main(String[] args){
+        // How many elements to be entered in array
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+
+        // User input for array elements
+        int arr[] = new int[N];
+        for(int i=0 ; i<N ; i++){
+            arr[i]=sc.nextInt();
+        }
+
+        // User input for the Key
+        int K = sc.nextInt();
+
+        int result = searchInSorted(arr,N,K);
+        System.out.println(result);
+    }
+}
