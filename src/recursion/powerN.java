98471eec10bb8e624c624843538f99bb7c6e6f64
Preetika Prakash <preetikaprakash3103@gmail.com>
2022-10-16 02:51:31 +0530

    optimized exponential recursion

--- src/recursion/powerN.java
+++ src/recursion/powerN.java
@@ -7,13 +7,33 @@ so powerN(3, 2) is 9 (3 squared).
 */
 package recursion;
 public class powerN {
+
+    // OPTIMIZED
+    public static int powerN_Optimized(int base, int n){
+        if(n==0){
+            return 1;
+        }
+        if(n%2==0){
+            return powerN_Optimized(base*base,n/2);
+        }
+        else{
+            return base*powerN_Optimized(base*base,(n-1)/2);
+        }
+
+    }
+    // BRUTE FORCE
     public static int powerN(int base, int n) {
         if(n==0){
             return 1; }
         else{
             return powerN(base,n-1)*base; } }
+
     public static void main(String[] args){
-        System.out.println("Value " +powerN(3,3) ); }
+
+        System.out.println("Value " +powerN(3,3) );
+
+        System.out.println("Value " +powerN_Optimized(2,9) ); }
 }
 // OUTPUT
 // Value 27
+// Value 512
