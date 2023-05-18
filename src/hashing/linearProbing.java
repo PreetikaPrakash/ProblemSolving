f9946a39c3d568d60e879b815b3738d7e516cbf4
Preetika Prakash <preetikaprakash3103@gmail.com>
2023-05-18 17:03:37 +0530

    linear probing

---
+++ src/hashing/linearProbing.java
@@ -0,0 +1,49 @@
+// Part of collision -> Open addressing -> linear probing, quardratic probing, double hashing
+
+// Insert and Search operations are performed below
+package hashing;
+
+import java.util.Arrays;
+
+public class linearProbing {
+    private static int SIZE=10;
+
+    public static int hashh(int key){
+        return key%SIZE;
+    }
+    public static void Insert(int[] HT, int key){
+        int index = hashh(key);
+        if(HT[index]!=0){
+            while(HT[index+1]%SIZE!=0){
+                index++;
+            }
+            int h = (index+1)%SIZE;
+            HT[h] = key;
+        }
+        else{
+            HT[index] = key;
+        }
+    }
+    public static int Search(int[] HT, int key){
+        int start_index=hashh(key);
+        int i=0;
+        while(HT[(start_index+i)%SIZE]!=key){
+            i++;
+        }
+        return (start_index+i)%SIZE;
+    }
+    public static void main(String[] args) {
+        // hashtable represented in array
+        int[] HT = new int[10];
+        Arrays.fill(HT,0);
+        Insert(HT,12);
+        Insert(HT,25);
+        Insert(HT,35);
+        Insert(HT,26);
+        int ss = Search(HT,35);
+        System.out.println("Key 35 found at "+ ss);
+        // OUTPUT
+        // Key 35 found at 6
+
+    }
+}
