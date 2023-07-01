968249e10554ac217baeaf85a4e9f70b83e56b07
Preetika Prakash <preetikaprakash3103@gmail.com>
2023-07-01 23:24:11 +0530

    Parenthesis Checker with output

--- src/stack/parenthesisChecker.java
+++ src/stack/parenthesisChecker.java
@@ -1,16 +1,13 @@
-// Implementing using array
+// Implemented Stack using array
 package stack;
-
-import java.util.Locale;
-
-class Stack{
+class PStack{
     int size;
     int top;
     char[] a;
 }
 public class parenthesisChecker {
-
-    static Stack stack = new Stack();
+    static PStack stack = new PStack();
+    static int newCount = 0;
 
     static void createStack(String x){
         stack.size = x.length();
@@ -19,14 +16,27 @@ public class parenthesisChecker {
     }
 
     static void push(char data){
-        int i=0;
         if(stack.top!=stack.size-1){
-            stack.a[i++] = data;
             stack.top++;
+            stack.a[stack.top] = data;
+            newCount++;
         }
+        // if stack is full
         else{
             System.out.println("StackOverflow");
         }
+    }
+    static char pop(){
+        char poped = ' ';
+        // if stack empty
+        if(stack.top==-1){
+            System.out.println("Stack Underflow");
+        }
+        else{
+            poped = stack.a[stack.top];
+            stack.top--;
+        }
+        return poped;
     }
 
     static boolean ispar(String x)
@@ -36,27 +46,72 @@ public class parenthesisChecker {
             // push open bracket
                 if(arr[i]=='('||arr[i]=='['||arr[i]=='{'){
                     push(arr[i]);
+                    System.out.println("Pushed element is " + arr[i]);
+                }
+                else if(arr[i]==')'|| arr[i]==']'||arr[i]=='}'){
+                    System.out.println("Poped element is " + pop());
                 }
             // pop open bracket when close bracket in arr
             // ignore other elements other than brackets
             // check - if at last open bracket is remaining - parenthesis not matching
             // check - if in stack while poping no open bracket present to pop - parenthesis not matching
         }
-        for(int i=0;i<stack.size;i++){
-            System.out.println(stack.a[i]);
+        stack.size = newCount;
+
+        // display stack
+        if(stack.top==-1){
+            System.out.println("Stack is empty");
+            System.out.println("Pair of brackets present");
+            return true;
         }
-        return false;
+        else{
+            System.out.println("Value present at stack top is " + stack.top);
+            return false;
+        }
+
     }
 
     public static void main(String[] args) {
         String str1 = "{([])}"; // true
         createStack(str1);
         System.out.println(ispar(str1));
-//        String str2 = "()"; // true
-//        System.out.println(ispar(str2));
-//        String str3 = "([]"; // false
-//        System.out.println(ispar(str3));
+
+        System.out.println("*************");
+        String str2 = "()"; // true
+        System.out.println(ispar(str2));
+
+        System.out.println("*************");
+        String str3 = "([]"; // false
+        System.out.println(ispar(str3));
+
     }
-
-
 }
+
+/*OUTPUT
+Pushed element is {
+Pushed element is (
+Pushed element is [
+Poped element is [
+Poped element is (
+Poped element is {
+Stack is empty
+Pair of brackets present
+true
+*************
+Pushed element is (
+Poped element is (
+Stack is empty
+Pair of brackets present
+true
+*************
+Pushed element is (
+Pushed element is [
+Poped element is [
+Value present at stack top is 0
+false
+
+
+
+
+
+* */
