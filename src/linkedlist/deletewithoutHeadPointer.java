3d36ca0e4bdc9a9faf50628915b9c60b70a086ef
Preetika Prakash <preetikaprakash3103@gmail.com>
2023-08-07 23:49:58 +0530

    Delete Node without Head Pointer

---
+++ src/linkedlist/deletewithoutHeadPointer.java
@@ -0,0 +1,38 @@
+package linkedlist;
+
+public class deletewithoutHeadPointer {
+
+    class Nodedelete
+    {
+        int data ;
+        Nodedelete next;
+        Nodedelete(int d)
+        {
+            data = d;
+            next = null;
+        }
+    }
+
+    //Function to delete a node without any reference to head pointer.
+    class Solution
+    {
+        void deleteNode(Node del)
+        {
+            del.data = del.next.data;
+            Node p = del.next;
+            del.next = p.next;
+
+        }
+    }
+}
+
+/*
+Input:
+N = 4
+value[] = {10,20,4,30}
+node = 20
+Output: 10 4 30
+Explanation: After deleting 20 from
+the linked list, we have remaining
+nodes as 10, 4 and 30.
+* */
