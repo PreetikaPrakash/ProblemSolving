a6f3c6190405b1753ffc30b75c76849bddf6c7d0
Preetika Prakash <preetikaprakash3103@gmail.com>
2023-08-15 23:52:35 +0530

    Roof Top

--- src/searching/roofTop.java
+++ src/searching/roofTop.java
@@ -1,30 +1,32 @@
 package searching;
 
 public class roofTop {
-    public static void main(String[] args) {
-        static int maxStep(int A[], int N)
-        {
-            int count = 0;
-            int zount = 0;
+    public static void main(String[] args) {}}
+//        static int maxStep(int A[], int N)
+//        {
+//            int count = 0;
+//            int zount = 0;
+//
+//
+//            for(int i=0;i<N-1;i++){
+//                if(A[i+1]>A[i])
+//
+//                {count++;
+//                    if(count>zount){
+//                        zount = count;
+//                    }}
+//                else{
+//                    count = 0;
+//                }
+//            }
+//            return zount;
+//        }
+//        //int res = maxStep(new int[]{1, 2, 3, 4}, 4));
+//
+//
+//    }
 
 
-            for(int i=0;i<N-1;i++){
-                if(A[i+1]>A[i])
-
-                {count++;
-                    if(count>zount){
-                        zount = count;
-                    }}
-                else{
-                    count = 0;
-                }
-            }
-            return zount;
-        }
-
-    }
-
-}
 
 /*
 * Input: N = 4 A[] = {1,2,3,4} Output: 3 Explanation: 1 to 2 to 3 to 4 is the jump of
