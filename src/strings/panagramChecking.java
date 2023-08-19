eec0bfb538b195162b58db1ccedfce01cd883491
Preetika Prakash <preetikaprakash3103@gmail.com>
2023-08-20 00:46:06 +0530

    Panagram Checking

--- src/strings/panagramChecking.java
+++ src/strings/panagramChecking.java
@@ -1,10 +1,14 @@
 package strings;
 
+import java.util.Locale;
+
 public class panagramChecking {
     public static boolean checkPangram  (String str) {
+
+        String str1 = str.toLowerCase(Locale.ROOT);
         int[] CharHash = new int[125];
-        char[] c = str.toCharArray();
-        boolean flag = true;
+        char[] c = str1.toCharArray();
+        boolean flag = false;
 
         //System.out.println(); // ASCII of B is 66, ASCII of 0 is 48, so result is 18
         for (int i = 0; i < c.length; i++) { // O(N)
@@ -12,25 +16,27 @@ public class panagramChecking {
             CharHash[ascii_letter]++;
         }
 
-        for (int j = 65; j < CharHash.length; j++) {
-            if ((j >= 65 || j <= 90) && (j >= 97 || j <= 122)) {
-                if (CharHash[j] > 1) {
-                    flag = true;
-                } else {
-                    flag = false;
-                }
-            } else {
-                continue;
+        for(int l=97;l<=122;l++){
+            if(CharHash[l]>=1){
+                flag = true;
             }
+            else{
+            flag = false;
+            break;
+        }
         }
 
-        if (flag == true) {
-            return true;
-        }
-        return false;
+        return flag;
     }
     public static void main(String[] args) {
         System.out.println(checkPangram  ("Bawds jog, flick quartz, vex nymph"));
+        System.out.println(checkPangram  ("eiegqcjtsjhqkmvwzedalkjdvmzgrxokchgoalguqcnqyrokqmjs"));
+
     }
 
 }
+
+/*OUTPUT
+* true
+* false
+* */
